09fd9243e8f2fe48f44818ccfbcf05bb7a4a502f
malik31200 <yka-vtc@outlook.fr>
2026-04-03 15:23:38 +0200

    fix pycodestyle syntaxe

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,21 +6,23 @@ The first box (box 0) is already unlocked.
 The function returns True if all boxes can be opened;
 otherwise, it returns False.
 """
+
+
 def canUnlockAll(boxes):
     # We create a set to keep the opened boxes
-    opened = set([0]) # The box 0 is opening at start
-    keys = [0] # We start with the key of the box 0
+    opened = set([0])  # The box 0 is opening at start
+    keys = [0]  # We start with the key of the box 0
 
     # While there are keys to explore
     while keys:
-        current = keys.pop() # we take the last box added
+        current = keys.pop()  # we take the last box added
 
         # we look all the keys find in the current box
         for key in boxes[current]:
-            # If the key corresponds to a real box and it hasn't been opened yet
-            if  key < len(boxes) and key not in opened:
-                opened.add(key) # we open the box
-                keys.append(key) # we add this box to explore later
+            # If the key corresponds to a real box and it hasnt been opened yet
+            if key < len(boxes) and key not in opened:
+                opened.add(key)  # we open the box
+                keys.append(key)  # we add this box to explore later
 
     # If all the boxes are opened, return True, otherwise False
     return len(opened) == len(boxes)
