b47d62b9dcd570c8de8e71c909f8f9351d1c5fb3
malik31200 <yka-vtc@outlook.fr>
2026-06-23 14:53:37 +0200

    add sanpiles.h

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,13 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdio.h>
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: first 3x3 grid
+ * @grid2: second 3x3 grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
