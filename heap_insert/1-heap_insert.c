fca72f17f93df52e6b2c3bed0de3a603591d1d24
malik31200 <yka-vtc@outlook.fr>
2026-06-23 14:29:13 +0200

    fix betty style

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -3,21 +3,16 @@
 
 /**
  * swap_values - swaps two integers
- * @a: first integer
- * @b: second integer
  */
 static void swap_values(int *a, int *b)
 {
-	int tmp;
-
-	tmp = *a;
+	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
 /**
- * heapify_up - restores Max Heap property going up
- * @node: pointer to the node to move up
+ * heapify_up - restores max heap property
  */
 static void heapify_up(heap_t *node)
 {
@@ -29,79 +24,62 @@ static void heapify_up(heap_t *node)
 }
 
 /**
- * get_size - returns number of nodes in heap
- * @root: root of heap
- *
- * Return: size of heap
+ * get_last_node - finds last level order node parent
  */
-static size_t get_size(const heap_t *root)
+static heap_t *get_last_parent(heap_t *root)
 {
-	if (root == NULL)
-		return (0);
+	heap_t *queue[1024];
+	int i = 0, j = 0;
+	heap_t *tmp;
 
-	return (1 + get_size(root->left) + get_size(root->right));
-}
+	queue[j++] = root;
 
-/**
- * get_node - finds insertion parent using index
- * @root: root of heap
- * @index: position in array form
- *
- * Return: pointer to parent node
- */
-static heap_t *get_node(heap_t *root, size_t index)
-{
-	size_t bits = 0;
-
-	if (root == NULL || index == 0)
-		return (NULL);
-
-	while ((1UL << bits) <= index)
-		bits++;
-
-	bits--;
-
-	while (bits > 0)
+	while (i < j)
 	{
-		bits--;
+		tmp = queue[i++];
 
-		if (index & (1UL << bits))
-			root = root->right;
+		if (tmp->left && tmp->right)
+		{
+			queue[j++] = tmp->left;
+			queue[j++] = tmp->right;
+		}
 		else
-			root = root->left;
+			return (tmp);
+
+		if (tmp->left)
+			queue[j++] = tmp->left;
+
+		if (tmp->right)
+			queue[j++] = tmp->right;
 	}
 
-	return (root);
+	return (NULL);
 }
 
 /**
  * heap_insert - inserts a value in Max Binary Heap
- * @root: double pointer to root
- * @value: value to insert
- *
- * Return: pointer to inserted node, or NULL
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *node;
 	heap_t *parent;
-	size_t size;
 
 	if (root == NULL)
 		return (NULL);
 
-	if (*root == NULL)
-	{
-		*root = binary_tree_node(NULL, value);
-		return (*root);
-	}
-
-	size = get_size(*root) + 1;
-	parent = get_node(*root, size / 2);
-
-	node = binary_tree_node(parent, value);
+	node = binary_tree_node(NULL, value);
 	if (node == NULL)
 		return (NULL);
+
+	if (*root == NULL)
+	{
+		*root = node;
+		return (node);
+	}
+
+	parent = get_last_parent(*root);
+
+	node->parent = parent;
 
 	if (parent->left == NULL)
 		parent->left = node;
