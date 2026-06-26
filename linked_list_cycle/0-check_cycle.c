38eccc00e44dd7f44ab12730536bcd643c96c0c4
malik31200 <yka-vtc@outlook.fr>
2026-06-26 15:09:44 +0200

    Linked list cycle

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -8,22 +8,22 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow, *fast;
+	listint_t *slow, *fast;
 
-    if (list == NULL)
-        return (0);
+	if (list == NULL)
+		return (0);
 
-    slow = list;
-    fast = list;
+	slow = list;
+	fast = list;
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-            return (1);
-    }
+		if (slow == fast)
+			return (1);
+	}
 
-    return (0);
+	return (0);
 }
