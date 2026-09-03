3112fbdb163e50fbf475fcd4c8b2407427262cbe
malik31200 <yka-vtc@outlook.fr>
2026-09-03 10:47:33 +0200

    Heap sort

---
+++ heap_sort/0-heap_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers
+ *
+ * @a: First integer
+ * @b: Second integer
+ */
+static void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * sift_down - Sifts an element down in a max heap
+ *
+ * @array: Array to modify
+ * @size: Size of the heap
+ * @root: Root index
+ */
+static void sift_down(int *array, size_t size, size_t root)
+{
+	size_t child;
+	size_t largest;
+
+	while (root * 2 + 1 < size)
+	{
+		child = root * 2 + 1;
+		largest = root;
+
+		if (array[child] > array[largest])
+			largest = child;
+
+		if (child + 1 < size && array[child + 1] > array[largest])
+			largest = child + 1;
+
+		if (largest == root)
+			return;
+
+		swap(&array[root], &array[largest]);
+		print_array(array, size);
+		root = largest;
+	}
+}
+
+/**
+ * heap_sort - Sorts an array using the Heap sort algorithm
+ *
+ * @array: Array of integers to sort
+ * @size: Number of elements in the array
+ */
+void heap_sort(int *array, size_t size)
+{
+	size_t i;
+
+	if (array == NULL || size < 2)
+		return;
+
+	i = size / 2;
+	while (i > 0)
+	{
+		i--;
+		sift_down(array, size, i);
+	}
+
+	i = size - 1;
+	while (i > 0)
+	{
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+		sift_down(array, i, 0);
+		i--;
+	}
+}
